7b4c743c11915ae90ea3acaf22a975eb684ca05e
shaunwong <kindoras@gmail.com>
2014-05-27 00:00:16 -0700

    initial commit

---
+++ public/script.js
@@ -0,0 +1,36 @@
+var Converter = require('csvtojson').core.Converter
+  , _         = require('lodash')
+  ;
+
+// jquery ajax using a promise
+$.get('data.csv').then(
+  function(data) {
+    parseCSV(data);
+  },
+  function() {
+    console.warn('file does nit exist');
+  }
+);
+
+var parseCSV = function(data) {
+  var converter = new Converter();
+  
+  converter.fromString(data,function(err, data){
+      if (err){
+        console.warn('error parsing file');
+      }
+      doShitWithData(data);
+  });
+};
+
+var doShitWithData = function(data) {
+  var dataEl  = $('#data tbody');
+  // lodash mustache template delims
+  _.templateSettings.interpolate = /{([\s\S]+?)}/g
+  _.forEach(data, function(row) {
+    var template = _.template('<tr><td>{name}</td><td>{category}</td><td>{weight}</td></tr>');
+    var output = template(row);
+    dataEl.append(output);
+    console.log(output);
+  });
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# example for dan
+
+## setup
+
+install deps
+
+```
+$ npm install
+$ npm build
+```
+This will install libs in `./node_modules` and browserify them to `public/bundle.js`.
+
+```
+$ npm start
+```
+
+This will start the static file server, serving files from `./public` to `//localhost:8080/`.
